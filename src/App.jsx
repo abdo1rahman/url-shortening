9ba9fa30eadcdd97feff594ce6f6113bdffa7bf8
abdo1rahman <abdoasem2055@gmail.com>
2025-06-26 12:01:32 +0300

    Navbar complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import Navbar from "./components/Navbar.jsx";
+
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  //
+  const [activeMenu, setActiveMenu] = useState("");
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  return (
+    <div className="app">
+      <Navbar
+        activeMenu={activeMenu}
+        setActiveMenu={setActiveMenu}
+        windowWidth={windowWidth}
+      />
+    </div>
+  );
 }
 
 export default App;
